7cb64587676d21fe3d4f7c293f015907d98f776b
Joe Meirow <joe.meirow@gmail.com>
2013-12-28 11:22:45 -0500

    remove then add column vs change data type

--- db/migrate/20131227032801_change_user_id_on_member_to_integer.rb
+++ db/migrate/20131227032801_change_user_id_on_member_to_integer.rb
@@ -1,5 +1,6 @@
 class ChangeUserIdOnMemberToInteger < ActiveRecord::Migration
   def change
+    remove_column :members, :user_id, :string
     change_column :members, :user_id, :integer
   end
 end
