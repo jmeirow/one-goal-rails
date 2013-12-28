68bdc044b9bee109ff5276334a5bf4e8cb517de0
Joe Meirow <joe.meirow@gmail.com>
2013-12-28 11:26:11 -0500

    doggone migration...

--- db/migrate/20131227032801_change_user_id_on_member_to_integer.rb
+++ db/migrate/20131227032801_change_user_id_on_member_to_integer.rb
@@ -1,6 +1,5 @@
 class ChangeUserIdOnMemberToInteger < ActiveRecord::Migration
   def change
-    remove_column :members, :user_id, :string
-    change_column :members, :user_id, :integer
+    add_column :members, :user_id, :integer
   end
 end
