06f315b950dc29d6d50a4573a102442cba9b69b8
Joe Meirow <joe.meirow@gmail.com>
2013-12-29 08:12:46 -0500

    make system text helper method 'text_for_key'

--- app/models/system_text.rb
+++ app/models/system_text.rb
@@ -3,4 +3,12 @@ class SystemText < ActiveRecord::Base
   paginates_per 5
 
 
+  def self.text_for_key key 
+    result  = SystemText.where("key = ?",key).first 
+    if result.nil?
+      "No System Text defined for key '#{key}'"
+    else
+      result.value
+    end
+  end
 end
