b97662710fa2d2437d5db99ffafd31da379cc851
Joe Meirow <joe.meirow@gmail.com>
2014-01-06 22:25:41 -0500

    changed migration because PostgreSQL cannot change a column's type

--- db/migrate/20140105205847_now_change_recommending_member_id_to_integer.rb
+++ db/migrate/20140105205847_now_change_recommending_member_id_to_integer.rb
@@ -1,5 +1,6 @@
 class NowChangeRecommendingMemberIdToInteger < ActiveRecord::Migration
   def change
-    change_column :web_resources, :recommending_member_id, :integer
+    remove_column :web_resources, :recommending_member_id, :string
+    add_column :web_resources, :recommending_member_id, :integer
   end
 end
